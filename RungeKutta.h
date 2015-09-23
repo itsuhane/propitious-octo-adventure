d6ea76903495d0f364ef2754ae84df29f6c7b4aa
Jinyu Li <itsuhane@gmail.com>
2015-09-23 11:22:28 +0800

    Add RungeKutta and UDPSocket

---
+++ RungeKutta.h
@@ -0,0 +1,129 @@
+#pragma once
+#include <vector>
+#include <functional>
+
+// A naive implementation based on wikipedia
+// may not good for serious application
+
+template<typename S, typename V>
+class RungeKutta {
+public:
+    RungeKutta() {
+        setRK4();
+    }
+
+    double integrate(const std::function<V(const S&, const V&)> &f, const V &y0, const S &t0, const S &t1) {
+        size_t s = stageNum();
+        std::vector<V> k(s, 0.0);
+        S h = t1 - t0;
+        V y1 = y0;
+        k[0] = f(t0, y0);
+        for (size_t i = 1; i < s; ++i) {
+            S ti = t0 + h*node(i);
+            V ks = 0;
+            for (size_t j = 0; j < i; ++j) {
+                S c = coefficient(i - 1, j);
+                if (c != 0) {
+                    ks += coefficient(i - 1, j)*k[j];
+                }
+            }
+            k[i] = f(ti, y0 + h*ks);
+        }
+        for (size_t i = 0; i < s; ++i) {
+            S w = weight(i);
+            if (w != 0) {
+                y1 += h*weight(i)*k[i];
+            }
+        }
+        return y1;
+    }
+
+    void setForwardEuler() {
+        setStageNum(1);
+        setWeight(0, 1);
+    }
+    void setSecondOrder(const S &x) {
+        setStageNum(2);
+        setNode(1, x);
+        setWeight(0, 1 - 0.5 / x);
+        setWeight(1, 0.5 / x);
+        setCoefficient(0, 0, x);
+    }
+    void setMidpoint() {
+        setSecondOrder(0.5);
+    }
+    void setHeun() {
+        setSecondOrder(1);
+    }
+    void setRalston() {
+        setSecondOrder(2.0 / 3.0);
+    }
+    void setRK3() {
+        setStageNum(3);
+        setNode(1, 0.5);
+        setNode(2, 1);
+        setWeight(0, 1.0 / 6.0);
+        setWeight(1, 2.0 / 3.0);
+        setWeight(2, 1.0 / 6.0);
+        setCoefficient(0, 0, 0.5);
+        setCoefficient(1, 0, -1);
+        setCoefficient(1, 1, 2);
+    }
+    void setRK4() {
+        setStageNum(4);
+        setNode(1, 0.5);
+        setNode(2, 0.5);
+        setNode(3, 1);
+        setWeight(0, 1.0 / 6.0);
+        setWeight(1, 1.0 / 3.0);
+        setWeight(2, 1.0 / 3.0);
+        setWeight(3, 1.0 / 6.0);
+        setCoefficient(0, 0, 0.5);
+        setCoefficient(1, 1, 0.5);
+        setCoefficient(2, 2, 1);
+    }
+
+    void set38RK4() {
+        setStageNum(4);
+        setNode(1, 1.0 / 3.0);
+        setNode(2, 2.0 / 3.0);
+        setNode(3, 1);
+        setWeight(0, 1.0 / 8.0);
+        setWeight(1, 3.0 / 8.0);
+        setWeight(2, 3.0 / 8.0);
+        setWeight(3, 1.0 / 8.0);
+        setCoefficient(0, 0, 1.0 / 3.0);
+        setCoefficient(1, 0, -1.0 / 3.0);
+        setCoefficient(1, 1, 1);
+        setCoefficient(2, 0, 1);
+        setCoefficient(2, 1, -1);
+        setCoefficient(2, 2, 1);
+    }
+
+    void setStageNum(size_t num) {
+        m_stages = num;
+        m_nodes.clear();
+        m_weights.clear();
+        m_coefficients.clear();
+        m_nodes.resize(num, 0);
+        m_weights.resize(num, 0);
+        m_coefficients.resize(num*(num - 1) / 2, 0);
+    }
+
+    size_t stageNum() const { return m_stages; }
+
+    void setNode(size_t n, const S &v) { m_nodes[n] = v; }
+    const S &node(size_t n) const { return m_nodes[n]; }
+
+    void setWeight(size_t w, const S &v) { m_weights[w] = v; }
+    const S &weight(size_t w) const { return m_weights[w]; }
+
+    void setCoefficient(size_t i, size_t j, const S &v) { m_coefficients[(i + 1)*i / 2 + j] = v; }
+    const S &coefficient(size_t i, size_t j) const { return m_coefficients[(i + 1)*i / 2 + j]; }
+
+private:
+    size_t m_stages;
+    std::vector<S> m_nodes;
+    std::vector<S> m_weights;
+    std::vector<S> m_coefficients;
+};
---
+++ UDPSocket.h
@@ -0,0 +1,215 @@
+#pragma once
+
+// From Gaffer on Games, it teaches many useful knowledge on
+// game physics and networking:
+// http://gafferongames.com/networking-for-game-programmers/
+
+#include <sstream>
+
+#define PLATFORM_WINDOWS 1
+#define PLATFORM_MAC     2
+#define PLATFORM_LINUX   3
+
+#if defined(_WIN32)
+#	define PLATFORM PLATFORM_WINDOWS
+#elif defined(__APPLE__)
+#	define PLATFORM PLATFORM_MAC
+#else
+#	define PLATFORM PLATFORM_LINUX
+#endif
+
+#if PLATFORM == PLATFORM_WINDOWS
+#	define _WINSOCK_DEPRECATED_NO_WARNINGS
+#	include <winsock2.h>
+#	include <ws2tcpip.h>
+#	pragma comment(lib, "Ws2_32.lib")
+#else
+#	include <sys/socket.h>
+#	include <netinet/in.h>
+#	include <fcntl.h>
+#endif
+
+namespace udp {
+
+	typedef unsigned long ipaddr;
+
+	class address {
+		friend class socket;
+	public:
+
+		// 0.0.0.0:0
+		address() : address(0, 0) {}
+
+		// 0.0.0.0:port
+		address(unsigned short port) : address(0, port) {}
+
+		// a.b.c.d:port
+		address(unsigned char a, unsigned char b, unsigned char c, unsigned char d, unsigned short port) : address((a << 24) | (b << 16) | (c << 8) | d, port) {}
+
+		// a.b.c.d:port
+		address(const std::string &ip, unsigned short port) : address(::htonl(::inet_addr(ip.c_str())), port) {}
+
+		// use together with resolv()
+		address(ipaddr host, unsigned short port) {
+			m_address.sin_family = AF_INET;
+			m_address.sin_addr.s_addr = ::htonl(host);
+			m_address.sin_port = ::htons(port);
+		}
+
+		// resolv("github.com") => address
+		// resolv("domain.not.exist") => INADDR_NONE
+		// use after socket::startup() and before socket::cleanup()
+		// otherwise INADDR_NONE will be returned always
+		static ipaddr resolv(const std::string &host) {
+			addrinfo *result = nullptr, hints;
+			ZeroMemory(&hints, sizeof(addrinfo));
+			hints.ai_family = AF_INET;
+			hints.ai_socktype = SOCK_DGRAM;
+			hints.ai_protocol = IPPROTO_UDP;
+			int ret = ::getaddrinfo(host.c_str(), nullptr, &hints, &result);
+			if (ret != 0) {
+				return INADDR_NONE;
+			}
+			ipaddr add = ::ntohl(((sockaddr_in*)result->ai_addr)->sin_addr.s_addr);
+			::freeaddrinfo(result);
+			return add;
+		}
+
+		std::string to_string() const {
+			std::stringstream ss;
+			ss << ::inet_ntoa(m_address.sin_addr) << ':' << ::ntohs(m_address.sin_port);
+			return ss.str();
+		}
+
+	private:
+		sockaddr_in m_address;
+	};
+
+	// use after socket::startup() and before socket::cleanup()
+	class socket {
+	public:
+		static bool startup() {
+#if PLATFORM == PLATFORM_WINDOWS
+			WSADATA wsaData;
+			int ret = ::WSAStartup(MAKEWORD(2, 2), &wsaData);
+			if (ret != 0) {
+				return false;
+			}
+#endif
+			return true;
+		}
+
+		static void cleanup() {
+#if PLATFORM == PLATFORM_WINDOWS
+			::WSACleanup();
+#endif
+		}
+
+		socket() {
+			m_socket = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+			if (m_socket == INVALID_SOCKET) {
+				throw "Cannot create non-blocking socket!";
+			}
+#if PLATFORM == PLATFORM_WINDOWS
+			DWORD non_blocking = 1;
+			int ret = ::ioctlsocket(m_socket, FIONBIO, &non_blocking);
+			if (ret != 0) {
+				throw "Cannot create non-blocking socket!";
+			}
+#else
+			int non_blocking = 1;
+			int ret = ::fcntl(m_socket, F_SETFL, O_NONBLOCK, non_blocking);
+			if (ret == -1) {
+				throw "Cannot create non-blocking socket!";
+			}
+#endif
+		}
+
+		~socket() {
+#if PLATFORM == PLATFORM_WINDOWS
+			::closesocket(m_socket);
+#else
+			::close(m_socket);
+#endif
+		}
+
+		socket(const socket&) = delete;
+
+		bool bind(const address &host) {
+			int ret = ::bind(m_socket, (const sockaddr*)&host.m_address, sizeof(sockaddr_in));
+			if (ret != 0) {
+				return false;
+			}
+			return true;
+		}
+
+		bool send(const address &dest, const void *data, size_t len) {
+			int sendlen = ::sendto(m_socket, (char*)data, (int)len, 0, (const sockaddr*)&dest.m_address, sizeof(sockaddr_in));
+			return sendlen == (int)len;
+		}
+
+		size_t recv(address &from, void *data, size_t maxlen) {
+			socklen_t addlen = sizeof(sockaddr_in);
+			int recvlen = ::recvfrom(m_socket, (char*)data, (int)maxlen, 0, (sockaddr*)&from.m_address, &addlen);
+			if (recvlen == SOCKET_ERROR) {
+				if (::WSAGetLastError() == WSAEWOULDBLOCK) {
+					recvlen = 0;
+				}
+				else {
+                    recvlen = 0;
+					//throw "Error receiving data!";
+				}
+			}
+			return (size_t)recvlen;
+		}
+
+	private:
+		SOCKET m_socket = INVALID_SOCKET;
+	};
+}
+
+/*
+	// Server example
+	#include <array>
+	#include <string>
+	#include <iostream>
+	#include <UDPSocket.h>
+
+	int main() {
+		udp::socket::startup();
+		udp::socket socket;
+		bool ret = socket.bind(udp::address(5911));
+		while (ret) {
+			udp::address address;
+			std::array<char, 256> buffer;
+			size_t recv_len = socket.recv(address, buffer.data(), buffer.size());
+			if (recv_len) {
+				std::string str(buffer.begin(), buffer.begin() + recv_len);
+				if (str == "<SHUTDOWN>") {
+					break;
+				}
+				std::cout << "from " << address.to_string() << " => " << str << std::endl;
+			}
+		}
+		udp::socket::cleanup();
+		return 0;
+	}
+*/
+
+/*
+	// Client example
+	#include <string>
+	#include <UDPSocket.h>
+
+	int main() {
+		std::string msg = "Hello, world!";
+
+		udp::socket::startup();
+		udp::socket socket;
+		udp::address dest(127, 0, 0, 1, 5911);
+		socket.send(dest, msg.data(), msg.size());
+		socket.send(dest, "<SHUTDOWN>", 10);
+		udp::socket::cleanup();
+		return 0;
+	}
+*/
