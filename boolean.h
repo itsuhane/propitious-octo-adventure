b2d2710252c65d3599ba31a1652e47764d67735c
itsuhane <itsuhane@users.noreply.github.com>
2018-01-24 17:26:24 +0800

    Add files via upload

--- boolean.h
+++ boolean.h
@@ -5,8 +5,10 @@
 struct boolean {
     boolean() = default;
     boolean(bool value) : value(value) {}
-    operator bool &() { return value; }
+    operator bool&() { return value; }
     operator const bool&() const { return value; }
+    bool* operator&() { return &value; }
+    const bool* operator&() const { return &value; }
 private:
     bool value;
 };
